45e26b6409fe05207370fc30d611c962f4962594
Calvin Bui <c@lvin.email>
2019-02-20 00:16:24 +1100

    updates

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -2,6 +2,5 @@
 alias sb='source $SB_TOOLKIT_DIR/load.sh'
 alias sb-ssh='python $SB_TOOLKIT_DIR/tools/sb/sb-ssh.py'
 alias sbsr-query='python /Users/calvin/repos/sb-toolkit/tools/sbsr/sbsr-query.py'
-alias cat='ccat'
 alias cdcustom='cd ~/.oh-my-zsh/custom'
 alias flushdns='sudo killall -HUP mDNSResponder'
