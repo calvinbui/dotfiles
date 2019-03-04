3134acf7e6073e86c0d50ebc0184e7494d654314
Calvin Bui <c@lvin.email>
2019-03-04 14:30:13 +1100

    March 4th update

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -2,6 +2,7 @@
 alias sb='source $SB_TOOLKIT_DIR/load.sh'
 alias sb-ssh='python $SB_TOOLKIT_DIR/tools/sb/sb-ssh.py'
 alias sbsr-query='python /Users/calvin/repos/sb-toolkit/tools/sbsr/sbsr-query.py'
+
 alias cdcustom='cd ~/.oh-my-zsh/custom'
 alias flushdns='sudo killall -HUP mDNSResponder'
 alias ubuntu='docker run --rm -it calvin:1804 bash'
---
+++ zsh/custom/functions.zsh
@@ -0,0 +1,4 @@
+# grep
+PATH="/usr/local/opt/grep/libexec/gnubin:$PATH"
+# curl
+PATH="/usr/local/opt/curl/bin:$PATH"
