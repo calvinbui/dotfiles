cc65aa0017c461722e0fed4fe17e6dec9071fe37
Calvin Bui <c@lvin.email>
2019-03-08 16:49:16 +1100

    weekly update

--- zsh/custom/functions.zsh
+++ zsh/custom/functions.zsh
@@ -2,3 +2,5 @@
 PATH="/usr/local/opt/grep/libexec/gnubin:$PATH"
 # curl
 PATH="/usr/local/opt/curl/bin:$PATH"
+# coreutils https://www.gnu.org/software/coreutils/
+PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
