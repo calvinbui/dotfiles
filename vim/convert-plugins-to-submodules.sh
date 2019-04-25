6e0ad8fe4f842686b6d6afa57d2ffe69774e68e9
Calvin Bui <c@lvin.email>
2019-04-25 14:01:01 +1000

    add vim submodules

--- vim/convert-plugins-to-submodules.sh
+++ vim/convert-plugins-to-submodules.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-for plugin in $(find ~/.vim/bundle/ -maxdepth 1 -mindepth 1 -type d -exec basename {} \;)
+for plugin in $(find ~/.vim/bundle/ -maxdepth 1 -mindepth 1 -type d)
 do
     pushd "$plugin"
     export url=$(git config --get remote.origin.url)
     popd
-    git submodule add "$url" vim/bundle/$plugin
+    git submodule add "$url" vim/bundle/$(basename $plugin)
 done
