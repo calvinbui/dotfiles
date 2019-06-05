9b35d6b8cfbfe26af9988ae31d27b97dfa16439d
Calvin Bui <3604363+calvinbui@users.noreply.github.com>
2019-06-05 14:58:35 +1000

    pip2/pip3 break up, stdin everything,

--- backup.sh
+++ backup.sh
@@ -4,7 +4,6 @@ atom/generate-package-list.sh
 mac/mackup-backup.sh --force
 mac/generate-brewfile.sh
 mac/defaults-backup.sh
-mac/macprefs-backup.sh
 pip/backup.sh
 npm/backup.sh
 # vim/convert-plugins-to-submodules.sh
--- pip/backup.sh
+++ pip/backup.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-pip freeze > $DIR/requirements.txt
+pip2 freeze > $DIR/pip2-requirements.txt
+pip3 freeze > $DIR/pip3-requirements.txt
