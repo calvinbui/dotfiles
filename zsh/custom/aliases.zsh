9aa87754cb16b3c48480f3d40edbe09c83c84baf
Calvin Bui <3604363+calvinbui@users.noreply.github.com>
2019-06-28 10:22:03 +1000

    zsh

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -1,7 +1,7 @@
 # sb-toolkit
 alias cdcustom='cd ~/.oh-my-zsh/custom'
+alias cdrepo='cd ~/repos/'
 alias flushdns='sudo killall -HUP mDNSResponder'
-alias ubuntu='docker run --rm -it calvin:1804 bash'
-
+alias ubuntu='docker run --rm -it ubuntu:latest bash'
 alias tf='terraform'
 alias tf11='terraform11'
--- zsh/custom/sb-toolkit.zsh
+++
@@ -1,2 +0,0 @@
-export SB_TOOLKIT_DIR='/Users/calvin/repos/sb-toolkit'
-#source $SB_TOOLKIT_DIR/load.sh
