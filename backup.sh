adaaa39537ab4534d8d130ea79f2c0a66be6cfee
Calvin Bui <3604363+calvinbui@users.noreply.github.com>
2020-01-18 01:24:35 +1100

    update

--- backup.sh
+++ backup.sh
@@ -4,7 +4,6 @@ set -e
 atom/backup.sh
 mac/mackup-backup.sh --force
 mac/generate-brewfile.sh
-mac/defaults-backup.sh
 pip/backup.sh
 npm/backup.sh
 # vim/convert-plugins-to-submodules.sh
--- zsh/custom/kubernetes.zsh
+++ zsh/custom/kubernetes.zsh
@@ -1,12 +1,8 @@
 alias k=kubectl
 
-alias kprod="kubectl config use-context gke_ap3-prod_us-central1_ap3-cluster-prod"
-alias kstg="kubectl config use-context gke_ap3-stg_australia-southeast1-b_ap3-cluster-stg"
-alias kops="kubectl config use-context gke_ap-ops_australia-southeast1-c_ops-cluster"
-
-alias kp="kubectl get pods"
-alias kcj="kubectl get cronjobs"
-alias ks="kubectl get service"
+alias kgp="kubectl get pods"
+alias kgcj="kubectl get cronjobs"
+alias kgs="kubectl get service"
 
 alias kg="kubectl get"
 alias kd="kubectl describe"
