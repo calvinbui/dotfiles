11772b4398c1cdb2241045a2b174b506d8d29ca6
Calvin Bui <3604363+calvinbui@users.noreply.github.com>
2020-05-03 16:59:58 +1000

    update

--- zsh/custom/dmgcrack.zsh
+++ zsh/custom/dmgcrack.zsh
@@ -14,3 +14,16 @@ dmgcrack(){
   echo "Setting empty password"
   printf '%s\0' "$password" '' | hdiutil chpass "${dmg}" -oldstdinpass -newstdinpass 
 }
+
+rar2john_cmd(){
+  echo "Generating hash file"
+  "$(dirname $(readlink -f /usr/local/bin/john))/rar2john" "$1"
+}
+
+rarcrack(){
+  rar="$1"
+  rar2john_cmd "${rar}" > "${rar}".hash
+  john --format=RAR5-opencl --mask="mac-torrent-download.net_[0-9a-z][0-9a-z][0-9a-z]" "${rar}".hash
+  password=$(john --show "${rar}".hash | sed '1q;d' | cut -d':' -f2)
+  echo "The password for $rar is $password"
+}
