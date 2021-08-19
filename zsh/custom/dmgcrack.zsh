557fc70accfd10bfc5c7e21dc86d80aa37bc03a9
Calvin Bui <3604363+calvinbui@users.noreply.github.com>
2021-08-19 16:08:22 +1000

    restore dmgcrack

---
+++ zsh/custom/dmgcrack.zsh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+dmg2john_cmd(){
+  echo "Generating hash file"
+  "$(dirname $(readlink -f /usr/local/bin/john))/dmg2john" "$1"
+}
+
+dmgcrack(){
+  dmg="$1"
+  dmg2john_cmd "${dmg}" > "${dmg}".hash
+  john --format=dmg-opencl --mask="mac-torrent-download.net_[0-9a-z][0-9a-z][0-9a-z]" "${dmg}".hash
+  password=$(john --show "${dmg}".hash | sed '1q;d' | cut -d':' -f2)
+  echo "The password for $dmg is $password"
+  echo "Setting empty password"
+  printf '%s\0' "$password" '' | hdiutil chpass "${dmg}" -oldstdinpass -newstdinpass
+  rm "${dmg}".hash
+}
+
+rar2john_cmd(){
+  echo "Generating hash file"
+  "$(dirname $(readlink -f /usr/local/bin/john))/rar2john" "$1"
+}
+
+rarcrack(){
+  rar="$1"
+  rar2john_cmd "${rar}" > "${rar}".hash
+  john --format=RAR5-opencl --mask="mac-torrent-download.net_[0-9a-z][0-9a-z][0-9a-z]" "${rar}".hash
+  password=$(john --show "${rar}".hash | sed '1q;d' | cut -d':' -f2)
+  echo "The password for $rar is $password"
+  rm "${rar}".hash
+}
