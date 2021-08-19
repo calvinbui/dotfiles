366ae6eabe40c8d83c6e6bf0900ff4630e7fa5b9
Calvin Bui <3604363+calvinbui@users.noreply.github.com>
2021-08-19 21:51:35 +1000

    only install missing atom packages

--- atom/backup.sh
+++ atom/backup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-apm list --installed --bare > $DIR/package-list.txt
+apm list --installed --bare > "${DIR}/package-list.txt"
