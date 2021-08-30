56e4196fffa6e45e6a1a81059f938d23f9d2bd95
Calvin Bui <3604363+calvinbui@users.noreply.github.com>
2021-08-30 15:11:59 +1000

    don't backup npm version

--- npm/backup.sh
+++ npm/backup.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-npm list -g --depth 0 | awk '{ print $2 }' | sed '/^$/d' >  $DIR/packages.txt
+npm list -g --depth 0 | awk '{ print $2 }' | sed '/^$/d' > "$DIR/packages.txt"
+sed -i /npm/d "$DIR/packages.txt"
