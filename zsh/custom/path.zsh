c0425a23d85db0fa239de7b7aebf00f2e43e4c9f
Calvin Bui <3604363+calvinbui@users.noreply.github.com>
2021-09-02 13:45:16 +1000

    edit path

--- zsh/custom/path.zsh
+++ zsh/custom/path.zsh
@@ -1,37 +1,37 @@
 if [ "$(uname -s)" = "Darwin" ]; then
     # grep
-    export PATH="${PATH}:/usr/local/opt/grep/libexec/gnubin"
+    export PATH="/usr/local/opt/grep/libexec/gnubin:${PATH}"
 
     # curl
-    export PATH="${PATH}:/usr/local/opt/curl/bin"
+    export PATH="/usr/local/opt/curl/bin:${PATH}"
 
     # ruby
-    export PATH="${PATH}:/usr/local/opt/ruby/bin"
+    export PATH="/usr/local/opt/ruby/bin:${PATH}"
 
     # gnu-tar
-    export PATH="${PATH}:/usr/local/opt/gnu-tar/libexec/gnubin"
+    export PATH="/usr/local/opt/gnu-tar/libexec/gnubin:${PATH}"
 
     # sed
-    export PATH="${PATH}:/usr/local/opt/gnu-sed/libexec/gnubin"
+    export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:${PATH}"
 
     # mysql
-    export PATH="${PATH}:/usr/local/opt/mysql-client/bin"
+    export PATH="/usr/local/opt/mysql-client/bin:${PATH}"
 
     # # go
-    # export PATH="${PATH}:${HOME}/go/bin/"
+    # export PATH="${HOME}/go/bin/:${PATH}"
 
     # coreutils https://www.gnu.org/software/coreutils/
-    export PATH="${PATH}:/usr/local/opt/coreutils/libexec/gnubin"
+    export PATH="/usr/local/opt/coreutils/libexec/gnubin:${PATH}"
 
     # findutils
-    export PATH="${PATH}:/usr/local/opt/findutils/libexec/gnubin"
+    export PATH="/usr/local/opt/findutils/libexec/gnubin:${PATH}"
 
     # openssl
-    export PATH="${PATH}:/usr/local/opt/openssl@1.1/bin"
+    export PATH="/usr/local/opt/openssl@1.1/bin:${PATH}"
 
     # homebrew
-    export PATH="${PATH}:/usr/local/sbin"
+    export PATH="/usr/local/sbin:${PATH}"
 
     # kubectl krew
-    export PATH="${PATH}:${HOME}/.krew/bin"
+    export PATH="${HOME}/.krew/bin:${PATH}"
 fi
