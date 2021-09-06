ffca6b350abdbbd991fed994e4ed255d922b3b47
Calvin Bui <3604363+calvinbui@users.noreply.github.com>
2021-09-06 15:30:12 +1000

    update git delete merge branches script

--- zsh/custom/git.zsh
+++ zsh/custom/git.zsh
@@ -1,6 +1,7 @@
 function git_delete_merged_branches {
   echo "Doing dry run"
   git remote prune origin --dry-run
+  git branch --merged | grep -v main | grep -v master | grep -v dev | grep -v public
 
   # POSIX compatible read
   printf "%s" "Continue? [y/n]: "
@@ -10,6 +11,7 @@ function git_delete_merged_branches {
     y)
       git remote prune origin
       git branch -vv | grep 'origin/.*: gone]' | awk '{print $1}' | xargs git branch -D
+      git branch --merged | grep -v main | grep -v master | grep -v dev | grep -v public | xargs -n 1 git branch -D
       ;;
     *) echo "no worries m8 cya" ;;
   esac
