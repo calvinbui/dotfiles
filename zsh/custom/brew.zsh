1d3b2ffb494a18581d7355aca0998364e500a43d
Calvin Bui <3604363+calvinbui@users.noreply.github.com>
2022-12-13 10:08:05 +1100

    update

--- zsh/custom/brew.zsh
+++ zsh/custom/brew.zsh
@@ -2,3 +2,6 @@ if [ "$(uname)" = "Darwin" ]; then
     export HOMEBREW_NO_ANALYTICS=1
 fi
 
+export HOMEBREW_CASK_OPTS="--no-quarantine"
+
+alias brew-cu="defaults export com.apple.dock "/tmp/dock-layout.plist" && brew cu -a && defaults import com.apple.dock "/tmp/dock-layout.plist" && killall Dock"
