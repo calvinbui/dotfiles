b97bf48c457820b27fbf7ef697083845418f7310
Calvin Bui <3604363+calvinbui@users.noreply.github.com>
2026-01-29 22:38:42 +1100

    delete vscode

--- backup.sh
+++ backup.sh
@@ -4,5 +4,4 @@ set -e
 mac/generate-brewfile.sh
 python/backup.sh
 npm/backup.sh
-vscode/backup-extensions.sh
 # vim/convert-plugins-to-submodules.sh
--- vscode/backup-extensions.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd
-
-code --list-extensions > extensions.txt
