4324dd2d4e8b7fe129a6d91288efcda55dbe4dcf
Calvin Bui <3604363+calvinbui@users.noreply.github.com>
2026-01-29 21:59:06 +1100

    change brewfile dump

--- mac/generate-brewfile.sh
+++ mac/generate-brewfile.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd
 rm Brewfile
-brew bundle dump
+brew bundle dump \
+  --no-vscode \
+  --no-go \
+  --no-cargo \
+  --no-flatpak
