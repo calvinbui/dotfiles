2ba433bd0c082dbcf0e265692d8e07f6f50a4d7e
Calvin Bui <3604363+calvinbui@users.noreply.github.com>
2026-01-29 22:09:22 +1100

    delete atom

--- atom/backup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-apm list --installed --bare > "${DIR}/package-list.txt"
--- backup.sh
+++ backup.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-atom/backup.sh
 mac/generate-brewfile.sh
 python/backup.sh
 npm/backup.sh
