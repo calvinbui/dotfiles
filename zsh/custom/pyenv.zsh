bc3a8d13d2f36896389611f05878c7ce3893d5c5
Calvin Bui <3604363+calvinbui@users.noreply.github.com>
2026-02-17 09:34:49 +1100

    update

--- zsh/custom/pyenv.zsh
+++ zsh/custom/pyenv.zsh
@@ -1,3 +1,3 @@
 export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init --path)"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init - zsh)"
